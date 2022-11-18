e2783ae46bc58a35ce766d999bc5dbe67bd50ac2
Tyken <117998041+Tyken73@users.noreply.github.com>
2022-11-18 11:51:41 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] times = new string[5];
+            int[] pontos = new int[5];
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("Digite o nome do {0}° time e sua pontuação: ", i+1);
+                times[i] = Console.ReadLine();
+                pontos[i] = int.Parse(Console.ReadLine());
+            }
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Time {0}: {1}, {2} pontos", i + 1, times[i], pontos[i]);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
